381ad04ae8d8eec9b41bfe8983cae07ddb12ed87
409411658 <409411658@gms.tku.edu.tw>
2022-03-31 21:55:22 +0800

    initial crown2_58 files

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 const crown_58_Router =require('./routes/crown_58');
+const crown2_58_Router =require('./routes/crown2_58');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/crown_58', crown_58_Router);
+app.use('/crown2_58', crown2_58_Router);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
