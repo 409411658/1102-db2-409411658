88fe5c95e7effa299c287bd70e9a071004d23b1d
409411658 <409411658@gms.tku.edu.tw>
2022-05-18 20:38:02 +0800

    p1:shop_58 and test fetchProductsByCategory(id)

---
+++ models/shop_58.js
@@ -0,0 +1,49 @@
+const db=require('../utils/database');
+
+const shop_58=class shop_58{
+
+    constructor(id,name,cat_id,price,remote_url,local_url){
+        this.id=id;
+        this.name=name;
+        this.cat_id=cat_id;
+        this.price=price;
+        this.remote_url=remote_url;
+        this.local_url=local_url;
+        
+    }
+
+    //get all categories
+
+    static async fetchAll(){
+        try{
+            let results=await db.query(`SELECT * from shop_58`);
+            //console.log('fetchAll results', JSON.stringify(results.rows));
+            return results.rows;
+        }catch(err){
+            console.log('error',err);
+        }
+    }
+
+    static async fetchProductsByShop(id){
+        const query={
+            text:`SELECT * from shop_58 where cat_id = $1`,
+            values: [id]
+        }
+        try{
+            let results=await db.query(query);
+            return results.rows;
+        }catch(err){
+            console.log(err);
+        }
+    }
+}
+
+//testing
+const test=async ()=>{
+    let results=await shop_58.fetchProductsByShop(1);
+    console.log('test results',JSON.stringify(results));
+}
+
+test();
+
+module.exports=shop_58;
