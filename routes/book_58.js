b61ef1044816d24e139319051457c1ef47da441a
409411658 <409411658@gms.tku.edu.tw>
2022-06-07 17:51:40 +0800

    w16-p4: POST /book_xx/create to create a book data

--- routes/book_58.js
+++ routes/book_58.js
@@ -3,7 +3,28 @@ var router = express.Router();
 
 const db=require('../utils/database');
 
+//create
+
+router.post('/create',async (req,res)=>{
+    console.log('body',req.body);
+    const id= req.body.id;
+    const name= req.body.name;
+    const author= req.body.author;
+    const price= req.body.price;
+
+    try{
+        const query ={
+            text:'INSERT INTO book_58(id,name,author,price) VALUES ($1,$2,$3,$4)',
+            values:[id,name,author,price],
+        };
+        await db.query(query);
+        res.redirect('/book_58');
+    }catch(error){
+        console.log(error);
+    }
+});
 /* GET home page. */
+//read
 router.get('/', async (req, res) =>{
     try{
         const results = await db.query('SELECT * FROM book_58');
