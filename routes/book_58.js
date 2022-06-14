ccf0fe17e9d0903641d32a55747ca33df686d2f6
409411658 <409411658@gms.tku.edu.tw>
2022-06-14 19:14:56 +0800

    w17-p7: implement /book_xx/update

--- routes/book_58.js
+++ routes/book_58.js
@@ -65,4 +65,43 @@ router.get('/delete/:id',async(req,res)=>{
     }
 });
 
+//update
+router.get('/update',async(req,res)=>{
+    try{
+        const query = {
+            text:`UPDATE book_58 SET name = $1, author = $2,
+            price = $3 WHERE id = $4;`,
+            values:[req.body.name,req.body.author,req.body.price,req.body.id],
+        };
+        await db.query(query);
+        res.redirect('/book_58');
+    }catch(err){
+        console.log(err);
+    }
+});
+
+
+//edit
+router.get('/edit/:id',async(req,res)=>{
+    const id=req.params.id;
+    console.log('id',id);
+    try{
+        const query = {
+            text:`SELECT * FROM book_58 WHERE id=$1;`,
+            values:[id],
+        };
+        const results = await db.query(query);
+        data=results.rows;
+        res.render('/book_58/edit_58',{
+            id:data[0].id,
+            name:data[0].name,
+            author:data[0].author,
+            price:data[0].price,
+
+        });
+    }catch(err){
+        console.log(err);
+    }
+});
+
 module.exports = router;
